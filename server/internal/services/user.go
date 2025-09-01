1f3f6beac360ae2c37c2f07586cb0b1f11a8cbaa
Samy Tamim <samytamim0@gmail.com>
2025-09-01 12:51:13 +0100

    fix: add missing LastName attribute

--- server/internal/services/user.go
+++ server/internal/services/user.go
@@ -25,6 +25,7 @@ func GetUsers() ([]*models.User, error) {
 
 type CreateUserType struct {
 	FirstName	string `validate:"required,min=4"`
+	LastName	string `validate:"required,min=4"`
 	Email    	string `validate:"required,email"`
 	Password	string `validate:"required,min=8,containsany=!@#?*"`
 }
