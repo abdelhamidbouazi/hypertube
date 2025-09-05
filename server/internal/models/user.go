849ab22299397c5717bb714da98cc9f66f1466d6
mylastresort <samytamim0@gmail.com>
2025-09-05 19:11:26 +0100

    feat: add OAuth2 for Google

--- server/internal/models/user.go
+++ server/internal/models/user.go
@@ -9,8 +9,18 @@ import (
 type User struct {
 	gorm.Model `json:"-"`
 	Tokens     pq.StringArray `gorm:"type:text[]" json:"-"`
+<<<<<<< Updated upstream
 	FirstName  string
 	LastName   string
 	Email      string `gorm:"uniqueIndex" json:"-"`
 	Password   string `json:"-"`
+=======
+	FirstName  string         `json:"firstname"`
+	LastName   string         `json:"lastname"`
+	Email      string         `json:"email"`
+	Password   string         `json:"-"`
+	Avatar     string         `json:"avatar"`
+	Provider   string         `json:"-"`
+	ProviderId string         `json:"-"`
+>>>>>>> Stashed changes
 }
---
+++ server/internal/services/users/createUser.go
@@ -0,0 +1,41 @@
+package users
+
+import (
+	"server/internal/models"
+	"server/internal/services"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm/clause"
+)
+
+type CreateUserType struct {
+	FirstName  string `validate:"required,min=4"`
+	LastName   string `validate:"required,min=4"`
+	Email      string `validate:"required,email"`
+	Password   string `validate:"required,min=8"`
+	Provider   string `validate:"required"`
+	ProviderId string
+	Avatar     string
+}
+
+func CreateUser(newUser CreateUserType) (models.User, error) {
+	db := services.PostgresDB()
+	var user models.User
+	user.Email = newUser.Email
+	user.FirstName = newUser.FirstName
+	user.LastName = newUser.LastName
+	user.Avatar = newUser.Avatar
+	hashed, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), 8)
+	if err != nil {
+		return user, err
+	}
+	user.Password = string(hashed)
+	user.Provider = newUser.Provider
+	user.ProviderId = newUser.ProviderId
+	res := db.Clauses(clause.Returning{}).Create(&user)
+	if res.Error != nil {
+		return user, res.Error
+	}
+	db.First(&user, user.ID)
+	return user, err
+}
