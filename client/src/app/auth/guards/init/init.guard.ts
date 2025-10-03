bd38d212687f8407565a6e2d085d5f3ddd5581d4
Abdelhamid Bouazi <abouazi@e2r4p13.1337.ma>
2025-10-03 15:57:14 +0100

    switching to nextjs

--- client/src/app/auth/guards/init/init.guard.ts
+++
@@ -1,8 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-import { inject } from '@angular/core';
-import { AuthService } from '~/app/auth/services/auth.service';
-
-export const initGuard: CanActivateFn = () => {
-  const auth = inject(AuthService);
-  return !auth.isLoading();
-};
